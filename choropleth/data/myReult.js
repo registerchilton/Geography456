b04ce5385d4652644bc58993dec93a6b61edf740
registerchilton <mchilton@outlook.com>
2024-01-31 17:01:00 -0500

    latest

---
+++ choropleth/data/myReult.js
@@ -0,0 +1,705 @@
+var myResult = [
+    {
+        "GEOID": "37001",
+        "NAME": "Alamance County",
+        "totalunits": "69749",
+        "mobileunits": "8342",
+        "ratio": "119"
+    },
+    {
+        "GEOID": "37003",
+        "NAME": "Alexander County",
+        "totalunits": "16399",
+        "mobileunits": "5111",
+        "ratio": "311"
+    },
+    {
+        "GEOID": "37005",
+        "NAME": "Alleghany County",
+        "totalunits": "8160",
+        "mobileunits": "1392",
+        "ratio": "170"
+    },
+    {
+        "GEOID": "37007",
+        "NAME": "Anson County",
+        "totalunits": "11594",
+        "mobileunits": "2929",
+        "ratio": "252"
+    },
+    {
+        "GEOID": "37009",
+        "NAME": "Ashe County",
+        "totalunits": "17649",
+        "mobileunits": "3820",
+        "ratio": "216"
+    },
+    {
+        "GEOID": "37011",
+        "NAME": "Avery County",
+        "totalunits": "14166",
+        "mobileunits": "2241",
+        "ratio": "158"
+    },
+    {
+        "GEOID": "37013",
+        "NAME": "Beaufort County",
+        "totalunits": "25930",
+        "mobileunits": "6963",
+        "ratio": "268"
+    },
+    {
+        "GEOID": "37015",
+        "NAME": "Bertie County",
+        "totalunits": "9841",
+        "mobileunits": "3607",
+        "ratio": "366"
+    },
+    {
+        "GEOID": "37017",
+        "NAME": "Bladen County",
+        "totalunits": "17877",
+        "mobileunits": "6624",
+        "ratio": "370"
+    },
+    {
+        "GEOID": "37019",
+        "NAME": "Brunswick County",
+        "totalunits": "86968",
+        "mobileunits": "19513",
+        "ratio": "224"
+    },
+    {
+        "GEOID": "37021",
+        "NAME": "Buncombe County",
+        "totalunits": "121591",
+        "mobileunits": "17321",
+        "ratio": "142"
+    },
+    {
+        "GEOID": "37023",
+        "NAME": "Burke County",
+        "totalunits": "41092",
+        "mobileunits": "10634",
+        "ratio": "258"
+    },
+    {
+        "GEOID": "37025",
+        "NAME": "Cabarrus County",
+        "totalunits": "78093",
+        "mobileunits": "4943",
+        "ratio": "63"
+    },
+    {
+        "GEOID": "37027",
+        "NAME": "Caldwell County",
+        "totalunits": "37950",
+        "mobileunits": "7215",
+        "ratio": "190"
+    },
+    {
+        "GEOID": "37029",
+        "NAME": "Camden County",
+        "totalunits": "4228",
+        "mobileunits": "662",
+        "ratio": "156"
+    },
+    {
+        "GEOID": "37031",
+        "NAME": "Carteret County",
+        "totalunits": "49911",
+        "mobileunits": "9057",
+        "ratio": "181"
+    },
+    {
+        "GEOID": "37033",
+        "NAME": "Caswell County",
+        "totalunits": "10778",
+        "mobileunits": "2904",
+        "ratio": "269"
+    },
+    {
+        "GEOID": "37035",
+        "NAME": "Catawba County",
+        "totalunits": "68693",
+        "mobileunits": "9668",
+        "ratio": "140"
+    },
+    {
+        "GEOID": "37037",
+        "NAME": "Chatham County",
+        "totalunits": "31555",
+        "mobileunits": "5203",
+        "ratio": "164"
+    },
+    {
+        "GEOID": "37039",
+        "NAME": "Cherokee County",
+        "totalunits": "18104",
+        "mobileunits": "3618",
+        "ratio": "199"
+    },
+    {
+        "GEOID": "37041",
+        "NAME": "Chowan County",
+        "totalunits": "7301",
+        "mobileunits": "1597",
+        "ratio": "218"
+    },
+    {
+        "GEOID": "37043",
+        "NAME": "Clay County",
+        "totalunits": "7309",
+        "mobileunits": "1437",
+        "ratio": "196"
+    },
+    {
+        "GEOID": "37045",
+        "NAME": "Cleveland County",
+        "totalunits": "43579",
+        "mobileunits": "8420",
+        "ratio": "193"
+    },
+    {
+        "GEOID": "37047",
+        "NAME": "Columbus County",
+        "totalunits": "26250",
+        "mobileunits": "8234",
+        "ratio": "313"
+    },
+    {
+        "GEOID": "37049",
+        "NAME": "Craven County",
+        "totalunits": "46911",
+        "mobileunits": "6393",
+        "ratio": "136"
+    },
+    {
+        "GEOID": "37051",
+        "NAME": "Cumberland County",
+        "totalunits": "145839",
+        "mobileunits": "13122",
+        "ratio": "89"
+    },
+    {
+        "GEOID": "37053",
+        "NAME": "Currituck County",
+        "totalunits": "15557",
+        "mobileunits": "2228",
+        "ratio": "143"
+    },
+    {
+        "GEOID": "37055",
+        "NAME": "Dare County",
+        "totalunits": "34515",
+        "mobileunits": "1925",
+        "ratio": "55"
+    },
+    {
+        "GEOID": "37057",
+        "NAME": "Davidson County",
+        "totalunits": "74311",
+        "mobileunits": "11424",
+        "ratio": "153"
+    },
+    {
+        "GEOID": "37059",
+        "NAME": "Davie County",
+        "totalunits": "18582",
+        "mobileunits": "3457",
+        "ratio": "186"
+    },
+    {
+        "GEOID": "37061",
+        "NAME": "Duplin County",
+        "totalunits": "25876",
+        "mobileunits": "9346",
+        "ratio": "361"
+    },
+    {
+        "GEOID": "37063",
+        "NAME": "Durham County",
+        "totalunits": "133429",
+        "mobileunits": "1836",
+        "ratio": "13"
+    },
+    {
+        "GEOID": "37065",
+        "NAME": "Edgecombe County",
+        "totalunits": "24945",
+        "mobileunits": "5961",
+        "ratio": "238"
+    },
+    {
+        "GEOID": "37067",
+        "NAME": "Forsyth County",
+        "totalunits": "163382",
+        "mobileunits": "6245",
+        "ratio": "38"
+    },
+    {
+        "GEOID": "37069",
+        "NAME": "Franklin County",
+        "totalunits": "28012",
+        "mobileunits": "7171",
+        "ratio": "255"
+    },
+    {
+        "GEOID": "37071",
+        "NAME": "Gaston County",
+        "totalunits": "91779",
+        "mobileunits": "10214",
+        "ratio": "111"
+    },
+    {
+        "GEOID": "37073",
+        "NAME": "Gates County",
+        "totalunits": "5336",
+        "mobileunits": "1735",
+        "ratio": "325"
+    },
+    {
+        "GEOID": "37075",
+        "NAME": "Graham County",
+        "totalunits": "6017",
+        "mobileunits": "1605",
+        "ratio": "266"
+    },
+    {
+        "GEOID": "37077",
+        "NAME": "Granville County",
+        "totalunits": "24069",
+        "mobileunits": "5033",
+        "ratio": "209"
+    },
+    {
+        "GEOID": "37079",
+        "NAME": "Greene County",
+        "totalunits": "8316",
+        "mobileunits": "3191",
+        "ratio": "383"
+    },
+    {
+        "GEOID": "37081",
+        "NAME": "Guilford County",
+        "totalunits": "226743",
+        "mobileunits": "6991",
+        "ratio": "30"
+    },
+    {
+        "GEOID": "37083",
+        "NAME": "Halifax County",
+        "totalunits": "25919",
+        "mobileunits": "5686",
+        "ratio": "219"
+    },
+    {
+        "GEOID": "37085",
+        "NAME": "Harnett County",
+        "totalunits": "51230",
+        "mobileunits": "11099",
+        "ratio": "216"
+    },
+    {
+        "GEOID": "37087",
+        "NAME": "Haywood County",
+        "totalunits": "35398",
+        "mobileunits": "6397",
+        "ratio": "180"
+    },
+    {
+        "GEOID": "37089",
+        "NAME": "Henderson County",
+        "totalunits": "56939",
+        "mobileunits": "8753",
+        "ratio": "153"
+    },
+    {
+        "GEOID": "37091",
+        "NAME": "Hertford County",
+        "totalunits": "10632",
+        "mobileunits": "2780",
+        "ratio": "261"
+    },
+    {
+        "GEOID": "37093",
+        "NAME": "Hoke County",
+        "totalunits": "20709",
+        "mobileunits": "4120",
+        "ratio": "198"
+    },
+    {
+        "GEOID": "37095",
+        "NAME": "Hyde County",
+        "totalunits": "3249",
+        "mobileunits": "546",
+        "ratio": "168"
+    },
+    {
+        "GEOID": "37097",
+        "NAME": "Iredell County",
+        "totalunits": "72834",
+        "mobileunits": "9889",
+        "ratio": "135"
+    },
+    {
+        "GEOID": "37099",
+        "NAME": "Jackson County",
+        "totalunits": "27073",
+        "mobileunits": "4810",
+        "ratio": "177"
+    },
+    {
+        "GEOID": "37101",
+        "NAME": "Johnston County",
+        "totalunits": "73685",
+        "mobileunits": "12796",
+        "ratio": "173"
+    },
+    {
+        "GEOID": "37103",
+        "NAME": "Jones County",
+        "totalunits": "4958",
+        "mobileunits": "1628",
+        "ratio": "328"
+    },
+    {
+        "GEOID": "37105",
+        "NAME": "Lee County",
+        "totalunits": "24463",
+        "mobileunits": "3531",
+        "ratio": "144"
+    },
+    {
+        "GEOID": "37107",
+        "NAME": "Lenoir County",
+        "totalunits": "27550",
+        "mobileunits": "6497",
+        "ratio": "235"
+    },
+    {
+        "GEOID": "37109",
+        "NAME": "Lincoln County",
+        "totalunits": "35227",
+        "mobileunits": "7410",
+        "ratio": "210"
+    },
+    {
+        "GEOID": "37111",
+        "NAME": "McDowell County",
+        "totalunits": "21297",
+        "mobileunits": "6643",
+        "ratio": "311"
+    },
+    {
+        "GEOID": "37113",
+        "NAME": "Macon County",
+        "totalunits": "25557",
+        "mobileunits": "3507",
+        "ratio": "137"
+    },
+    {
+        "GEOID": "37115",
+        "NAME": "Madison County",
+        "totalunits": "10892",
+        "mobileunits": "2796",
+        "ratio": "256"
+    },
+    {
+        "GEOID": "37117",
+        "NAME": "Martin County",
+        "totalunits": "11608",
+        "mobileunits": "2532",
+        "ratio": "218"
+    },
+    {
+        "GEOID": "37119",
+        "NAME": "Mecklenburg County",
+        "totalunits": "435795",
+        "mobileunits": "6149",
+        "ratio": "14"
+    },
+    {
+        "GEOID": "37121",
+        "NAME": "Mitchell County",
+        "totalunits": "8812",
+        "mobileunits": "1552",
+        "ratio": "176"
+    },
+    {
+        "GEOID": "37123",
+        "NAME": "Montgomery County",
+        "totalunits": "16235",
+        "mobileunits": "3737",
+        "ratio": "230"
+    },
+    {
+        "GEOID": "37125",
+        "NAME": "Moore County",
+        "totalunits": "46638",
+        "mobileunits": "6965",
+        "ratio": "149"
+    },
+    {
+        "GEOID": "37127",
+        "NAME": "Nash County",
+        "totalunits": "42876",
+        "mobileunits": "8242",
+        "ratio": "192"
+    },
+    {
+        "GEOID": "37129",
+        "NAME": "New Hanover County",
+        "totalunits": "109244",
+        "mobileunits": "4472",
+        "ratio": "40"
+    },
+    {
+        "GEOID": "37131",
+        "NAME": "Northampton County",
+        "totalunits": "11670",
+        "mobileunits": "2621",
+        "ratio": "224"
+    },
+    {
+        "GEOID": "37133",
+        "NAME": "Onslow County",
+        "totalunits": "78501",
+        "mobileunits": "11375",
+        "ratio": "144"
+    },
+    {
+        "GEOID": "37135",
+        "NAME": "Orange County",
+        "totalunits": "57502",
+        "mobileunits": "4799",
+        "ratio": "83"
+    },
+    {
+        "GEOID": "37137",
+        "NAME": "Pamlico County",
+        "totalunits": "7721",
+        "mobileunits": "1970",
+        "ratio": "255"
+    },
+    {
+        "GEOID": "37139",
+        "NAME": "Pasquotank County",
+        "totalunits": "17108",
+        "mobileunits": "2089",
+        "ratio": "122"
+    },
+    {
+        "GEOID": "37141",
+        "NAME": "Pender County",
+        "totalunits": "28601",
+        "mobileunits": "6773",
+        "ratio": "236"
+    },
+    {
+        "GEOID": "37143",
+        "NAME": "Perquimans County",
+        "totalunits": "7136",
+        "mobileunits": "1587",
+        "ratio": "222"
+    },
+    {
+        "GEOID": "37145",
+        "NAME": "Person County",
+        "totalunits": "18428",
+        "mobileunits": "4173",
+        "ratio": "226"
+    },
+    {
+        "GEOID": "37147",
+        "NAME": "Pitt County",
+        "totalunits": "78563",
+        "mobileunits": "9528",
+        "ratio": "121"
+    },
+    {
+        "GEOID": "37149",
+        "NAME": "Polk County",
+        "totalunits": "11674",
+        "mobileunits": "1750",
+        "ratio": "149"
+    },
+    {
+        "GEOID": "37151",
+        "NAME": "Randolph County",
+        "totalunits": "62147",
+        "mobileunits": "13428",
+        "ratio": "216"
+    },
+    {
+        "GEOID": "37153",
+        "NAME": "Richmond County",
+        "totalunits": "21380",
+        "mobileunits": "5574",
+        "ratio": "260"
+    },
+    {
+        "GEOID": "37155",
+        "NAME": "Robeson County",
+        "totalunits": "53065",
+        "mobileunits": "20342",
+        "ratio": "383"
+    },
+    {
+        "GEOID": "37157",
+        "NAME": "Rockingham County",
+        "totalunits": "43977",
+        "mobileunits": "7412",
+        "ratio": "168"
+    },
+    {
+        "GEOID": "37159",
+        "NAME": "Rowan County",
+        "totalunits": "61329",
+        "mobileunits": "10115",
+        "ratio": "164"
+    },
+    {
+        "GEOID": "37161",
+        "NAME": "Rutherford County",
+        "totalunits": "34275",
+        "mobileunits": "7127",
+        "ratio": "207"
+    },
+    {
+        "GEOID": "37163",
+        "NAME": "Sampson County",
+        "totalunits": "27610",
+        "mobileunits": "10152",
+        "ratio": "367"
+    },
+    {
+        "GEOID": "37165",
+        "NAME": "Scotland County",
+        "totalunits": "15295",
+        "mobileunits": "3599",
+        "ratio": "235"
+    },
+    {
+        "GEOID": "37167",
+        "NAME": "Stanly County",
+        "totalunits": "27638",
+        "mobileunits": "3609",
+        "ratio": "130"
+    },
+    {
+        "GEOID": "37169",
+        "NAME": "Stokes County",
+        "totalunits": "22186",
+        "mobileunits": "5530",
+        "ratio": "249"
+    },
+    {
+        "GEOID": "37171",
+        "NAME": "Surry County",
+        "totalunits": "34030",
+        "mobileunits": "8366",
+        "ratio": "245"
+    },
+    {
+        "GEOID": "37173",
+        "NAME": "Swain County",
+        "totalunits": "8951",
+        "mobileunits": "2451",
+        "ratio": "273"
+    },
+    {
+        "GEOID": "37175",
+        "NAME": "Transylvania County",
+        "totalunits": "19560",
+        "mobileunits": "2737",
+        "ratio": "139"
+    },
+    {
+        "GEOID": "37177",
+        "NAME": "Tyrrell County",
+        "totalunits": "2229",
+        "mobileunits": "676",
+        "ratio": "303"
+    },
+    {
+        "GEOID": "37179",
+        "NAME": "Union County",
+        "totalunits": "79644",
+        "mobileunits": "4666",
+        "ratio": "58"
+    },
+    {
+        "GEOID": "37181",
+        "NAME": "Vance County",
+        "totalunits": "20117",
+        "mobileunits": "4808",
+        "ratio": "239"
+    },
+    {
+        "GEOID": "37183",
+        "NAME": "Wake County",
+        "totalunits": "421426",
+        "mobileunits": "13644",
+        "ratio": "32"
+    },
+    {
+        "GEOID": "37185",
+        "NAME": "Warren County",
+        "totalunits": "11948",
+        "mobileunits": "3798",
+        "ratio": "317"
+    },
+    {
+        "GEOID": "37187",
+        "NAME": "Washington County",
+        "totalunits": "6458",
+        "mobileunits": "1542",
+        "ratio": "238"
+    },
+    {
+        "GEOID": "37189",
+        "NAME": "Watauga County",
+        "totalunits": "33520",
+        "mobileunits": "2936",
+        "ratio": "87"
+    },
+    {
+        "GEOID": "37191",
+        "NAME": "Wayne County",
+        "totalunits": "54074",
+        "mobileunits": "13379",
+        "ratio": "247"
+    },
+    {
+        "GEOID": "37193",
+        "NAME": "Wilkes County",
+        "totalunits": "33480",
+        "mobileunits": "9235",
+        "ratio": "275"
+    },
+    {
+        "GEOID": "37195",
+        "NAME": "Wilson County",
+        "totalunits": "36005",
+        "mobileunits": "4873",
+        "ratio": "135"
+    },
+    {
+        "GEOID": "37197",
+        "NAME": "Yadkin County",
+        "totalunits": "17401",
+        "mobileunits": "4508",
+        "ratio": "259"
+    },
+    {
+        "GEOID": "37199",
+        "NAME": "Yancey County",
+        "totalunits": "11211",
+        "mobileunits": "2427",
+        "ratio": "216"
+    },
+    {
+        "GEOID": ""
+    }
+]
